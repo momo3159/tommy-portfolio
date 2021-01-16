79dc4a4cbddb181490b8ef3f441b54a6b406dcdd
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2021-01-17 08:58:17 +0900

    lバグの修正

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,33 +5,33 @@
       <v-row justify="center">
         <v-col align="center">
           <v-card max-width="400">
-            <v-card-title class="justify-center card-title"
-              >{{aboutMe.name}}</v-card-title
-            >
+            <v-card-title class="justify-center card-title">{{
+              aboutMe.name
+            }}</v-card-title>
             <v-card-text>
-              {{aboutMe.univ}} <br/>
-              {{aboutMe.hobby}} <br />
-              {{aboutMe.rank}} <br />
-              {{aboutMe.email}}
+              {{ aboutMe.univ }} <br />
+              {{ aboutMe.hobby }} <br />
+              {{ aboutMe.rank }} <br />
+              {{ aboutMe.email }}
             </v-card-text>
             <v-card-actions class="justify-center">
-              <a href="aboutMe.github.url">
+              <a :href="aboutMe.github.url" target="_blank">
                 <v-btn icon>
                   <v-fa :icon="['fab', 'github']" size="2x"></v-fa>
                 </v-btn>
               </a>
-              <a href="aboutMe.twitter.url">
+              <a :href="aboutMe.twitter.url" target="_blank">
                 <v-btn icon>
                   <v-fa :icon="['fab', 'twitter']" size="2x"></v-fa>
                 </v-btn>
               </a>
-              <a href="aboutMe.atcoder.url">
+              <a :href="aboutMe.atcoder.url" target="_blank">
                 <v-btn outlined color="grey darken-2" small rounded>
                   <v-fa :icon="['fa', 'link']" size="lg"></v-fa>
                   AtCoder</v-btn
                 >
               </a>
-              <a href="aboutMe.qiita.url">
+              <a :href="aboutMe.qiita.url" target="_blank">
                 <v-btn outlined color="grey darken-2" small rounded>
                   <v-fa :icon="['fa', 'link']" size="lg"></v-fa>
                   Qiita
@@ -65,8 +65,8 @@ export default {
         atcoder: {
           url: "https://atcoder.jp/users/Tommy3",
         },
-        qiita: {  
-          url: "https://qiita.com/tommy3" 
+        qiita: {
+          url: "https://qiita.com/tommy3",
         },
       },
     };
